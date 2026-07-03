7c53af8169572fd0cfd84eb7185e18d34869f185
Humza H <32408500+MDoubleH@users.noreply.github.com>
2026-07-03 22:16:23 +0100

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        '''
+        TC: O(n*m) 
+        n = number of strings
+m = length of the first string / prefix length checked
+        SC: O(1)
+        '''
+        res = ""
+
+        #obtain the index we want to go through
+        for i in range(len(strs[0])):
+            #go through that index for all our strings
+            for s in strs:
+                #if we reach the end of a str or no longer match, return
+                if i == len(s) or strs[0][i] != s[i]:
+                    return res
+            res += s[i]
+
+        return res
+
